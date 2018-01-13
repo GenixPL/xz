362aa571467d065a923696ce4bf8dbe868708ca6
GenixPl <intergenixyt@gmail.com>
2018-01-13 13:58:00 +0100

    toast added

--- app/src/main/java/pw/xz/xdd/xz/MainActivity.java
+++ app/src/main/java/pw/xz/xdd/xz/MainActivity.java
@@ -3,9 +3,11 @@ package pw.xz.xdd.xz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.indoorway.android.common.sdk.IndoorwaySdk;
 import com.indoorway.android.common.sdk.listeners.generic.Action1;
+import com.indoorway.android.common.sdk.model.Coordinates;
 import com.indoorway.android.common.sdk.model.IndoorwayMap;
 import com.indoorway.android.map.sdk.view.IndoorwayMapView;
 
@@ -26,10 +28,26 @@ public class MainActivity extends AppCompatActivity {
         IndoorwaySdk.configure("6513a6eb-133a-43f6-a206-6afe3e07fe96");
 
         indoorwayMapView = findViewById(R.id.mapView);
+        initializeMap();
 
-        indoorwayMapView.load("CScrSxCVhQg", "3-_M01M3r5w");
         RoomProximityDetector detector = new RoomProximityDetector(indoorwayMapView);
+
 
     }
 
+    private void initializeMap(){
+        indoorwayMapView.load("CScrSxCVhQg", "3-_M01M3r5w");
+
+        indoorwayMapView.getTouch().setOnClickListener(new Action1<Coordinates>() {
+            @Override
+            public void onAction(Coordinates coordinates) {
+                toastMessage(coordinates.toString());
+            }
+        });
+    }
+
+
+    public void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
