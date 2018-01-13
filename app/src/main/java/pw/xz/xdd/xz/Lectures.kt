87023b8ef4e1e91d30d6f6808311ad40d14a894e
emilekm2142 <emilm7843@gmail.com>
2018-01-13 16:37:05 +0100

    duaoaa

---
+++ app/src/main/java/pw/xz/xdd/xz/Lectures.kt
@@ -0,0 +1,12 @@
+package pw.xz.xdd.xz
+
+/**
+ * Created by emile on 13-Jan-18.
+ */
+data class Lectures(
+        var name:String,
+        var startHour:String,
+        var roomId:String,
+        var endHour:String,
+        var day:String
+)
--- app/src/main/java/pw/xz/xdd/xz/RoomProximityDetector.kt
+++ app/src/main/java/pw/xz/xdd/xz/RoomProximityDetector.kt
@@ -3,11 +3,11 @@ package pw.xz.xdd.xz
 import com.indoorway.android.common.sdk.listeners.generic.Action1
 import com.indoorway.android.common.sdk.model.Coordinates
 import com.indoorway.android.common.sdk.model.IndoorwayMap
-
+import com.indoorway.android.common.sdk.model.proximity.IndoorwayNotificationInfo
+import com.indoorway.android.common.sdk.model.proximity.IndoorwayProximityEvent
+import com.indoorway.android.common.sdk.model.proximity.IndoorwayProximityEventShape
 import com.indoorway.android.location.sdk.IndoorwayLocationSdk
 import com.indoorway.android.map.sdk.view.IndoorwayMapView
-import com.indoorway.android.map.sdk.view.MapView
-import com.indoorway.android.common.sdk.model.proximity.IndoorwayProximityEvent
 
 
 /**
@@ -37,6 +37,8 @@ class RoomProximityDetector(
         for (mapObject in map.objects ){
             var isAuditory = false
             var isRoom = true
+
+            //tutaj sa dirty hacki bo pokoje nie maja tagow??
             if (mapObject.name!!.toLowerCase().contains("room")){
 
             }
@@ -47,9 +49,18 @@ class RoomProximityDetector(
             roomsList.add(thisRoom)
         }
     }
-    fun getNearestRoom(){
-
+    fun getNearestRoom(position:Coordinates):Pair<Room?, Double> {
+        var minDistance: Double = 999999.0;
+        var nearestRoom: Room? = null;
+        for (room in roomsList) {
+            if (room.coordinatesCenter.getDistanceTo(position) < minDistance) {
+                minDistance = room.coordinatesCenter.getDistanceTo(position)
+                nearestRoom = room
+            }
+        }
+        return Pair(nearestRoom,minDistance)
     }
+
     fun makeTriggersFromRooms(){
         //Nie znam inputu danych z sqla
 
@@ -59,7 +70,20 @@ class RoomProximityDetector(
 
     }
     public fun registerTrigger(){
-
+        //redudant?
+        IndoorwayLocationSdk.instance().customProximityEvents()
+                .add(IndoorwayProximityEvent(
+                        "proximity-event-id", // identifier
+                        IndoorwayProximityEvent.Trigger.ENTER, // trigger on enter or on exit?
+                        IndoorwayProximityEventShape.Circle(
+                                Coordinates(51.0, 20.0),
+                                3.0
+                        ),
+                        "building-uuid", // building identifier
+                        "map-uuid", // map identifier
+                        0L, // (optional) timeout to show notification, will be passed as parapeter to listener
+                        IndoorwayNotificationInfo("title", "description") // (optional) data to show in notification
+                ))
     }
     public fun onEnterZone(event: IndoorwayProximityEvent){
 
