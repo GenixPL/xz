5e9644bd6214f528ccfac7bc2e12fae5380d799c
emilekm2142 <emilm7843@gmail.com>
2018-01-13 20:15:42 +0100

    dupa

--- app/src/main/java/pw/xz/xdd/xz/JavaDisabilitiesFixer.kt
+++ app/src/main/java/pw/xz/xdd/xz/JavaDisabilitiesFixer.kt
@@ -5,6 +5,20 @@ import java.util.*
 /**
  * Created by emile on 13-Jan-18.
  */
+// no ale ze w javie nie mozna robic dictionary to bym nie pomyslal xdddd
+
+//dla javowcw co musza wszystko miec w funkcjach jednolinikowych :/
+fun getDayFromCalendarEnum(calendarDay:Int):String?{
+    return days[calendarDay]
+}
+fun getCalendarEnumFromDay(day:String):Int?{
+    for ((key,value) in days){
+        if (value == day){
+            return key
+        }
+    }
+    return null
+}
 var days = mapOf<Int, String>(
         Calendar.MONDAY to "Monday",
         Calendar.TUESDAY to "Tuesday",
